c5068ea5ecba5df57e9cd328bfd332cbbe700ea2
Marcelo Molinas <federico.marcelo.molinas@gmail.com>
2021-08-29 15:32:53 -0300

    segundo commit src files added

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,41 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
+    <section class="showcase">
+			<div class="video-container">
+				<video src="" autoplay muted loop></video>
+			</div>
+			<div class="content">
+				<h1>Shoot For The Stars</h1>
+				<h3>Full screen video landing page</h3>
+				<a href="#about" class="btn">Read More</a>
+			</div>
+		</section>
+
+		<section id="about">
+			<h1>About</h1>
+			<p>
+				This is a landing page with a full screen video background. Feel free to
+				use this landing page in your projects. keep adding sections, change the
+				video, content , etc
+			</p>
+
+			<h2>Follow Me On Social Media</h2>
+
+			<div class="social">
+			<a href="https://twitter.com/traversymedia" target="_blank"><i class="fab fa-twitter fa-3x"></i></a>
+				<a href="https://facebook.com/traversymedia" target="_blank"><i class="fab fa-facebook fa-3x"></i></a>
+				<a href="https://github.com/bradtraversy" target="_blank"><i class="fab fa-github fa-3x"></i></a>
+				<a href="https://www.linkedin.com/in/bradtraversy" target="_blank"><i class="fab fa-linkedin fa-3x"></i></a>
+			</div>
+		</section>
+
+
+
+    
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    
   </Layout>
 )
 
